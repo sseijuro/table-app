fca35b96bbf9402bc5afde37f1d5d2689a9b3214
Kozorez Alexandr <kozorez.tech@gmail.com>
2022-02-07 16:25:57 +0300

    table-app

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # table-app
+![Screenshot](https://raw.github.com/seijuroseta/table-app/master/preview.png)
---
+++ table-app/Setting.swift
@@ -0,0 +1,35 @@
+//
+//  Setting.swift
+//  table-app
+//
+//  Created by Alexandr Kozorez on 07.02.2022.
+//
+
+import Foundation
+
+struct Setting {
+    var avatar: URL? = nil
+    let title: String
+    var subtitle: String? = nil
+    var caption: String? = nil
+    var isActive: Bool = false
+}
+
+extension Setting {
+    static var defaultSettingsList: [Setting] {
+        [
+            Setting(title: "Title"),
+            Setting(title: "Title", subtitle: "Subtitle"),
+            Setting(title: "Title", subtitle: loremOneParagraph),
+            Setting(title: "Title", subtitle: loremOneParagraph, caption: "Caption"),
+            Setting(title: loremOneParagraph),
+            Setting(title: loremOneParagraph, subtitle: "Subtitle"),
+            Setting(title: loremOneParagraph, subtitle: loremOneParagraph),
+            Setting(title: loremOneParagraph, subtitle: loremOneParagraph, caption: "Caption")
+        ]
+    }
+    
+    static var loremOneParagraph: String {
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    }
+}
